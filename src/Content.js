aef93df26d1c90035a3ed5ed1f477c4f45172a31
Lyana MALAYA <lmalaya@e1r11p5.unit.ua>
2019-09-21 22:26:25 +0300

    promis

--- src/Content.js
+++ src/Content.js
@@ -2,9 +2,8 @@ import React from "react";
 import {Sonnet, Container, Row, Col, Button, Tabs,  Tab, Nav, ButtonGroup,DropdownButton, Dropdown } from "react-bootstrap";
 import cmxAPI from "./cmxAPI";
 import Select from 'react-select';
-
-import AWS from 'awesome-notifications';
-
+import AWN from 'awesome-notifications';
+import "awesome-notifications/dist/style.css";
 
 const options = [
     { value: '1st_Floor', label: 'First' },
@@ -15,24 +14,45 @@ const options = [
 class Content extends React.Component  {
     state = {
         image: "",
-        selectedFloor: { value: '1st_Floor', label: 'First' }
+        selectedFloor: { value: '1st_Floor', label: 'First' },
+        clients: [],
+        connected: "",
+        all: "",
+        detected: ""
     }
    
     componentDidMount() {
         cmxAPI.getFloorImage(this.state.selectedFloor.value, image => this.setState({ image }));
+        let notifier = new AWN({});
+        setInterval(async () => {
+            let oldClients = this.state.clients,
+                newClients = await cmxAPI.getAllClients();
+            this.setState({clients: newClients});
+            if (oldClients.length == 0) {
+                oldClients = newClients;
+            }
+            // console.log("newww:",newClients, "old", oldClients);
+            var newMacAdresses = this.getNewMacAdresses(oldClients, newClients);
+            // console.log("newww",newMacAdresses);
+            newMacAdresses.forEach(function(macAddr){
+                notifier.info("mac Address: " + macAddr, {});
+            });
+        }, 1000);
+    }
+
+    macAdressesFromClients(clients) {
+        return clients.map(client => client.macAddress);
+    }
+
+    getNewMacAdresses(oldClients, updatedClients){
+        oldClients = this.macAdressesFromClients(oldClients)
+        updatedClients = this.macAdressesFromClients(updatedClients)
+        return updatedClients.filter(e => !oldClients.includes(e));
     }
 
     handleFloorChange = selectedFloor => {
         cmxAPI.getFloorImage(selectedFloor.value, image => this.setState({ image }));
         this.setState({ selectedFloor });
-
-        addNotification({
-            title: 'Welcome',
-            message: 'you clicked on the button',
-            level: 'success',
-            dismissible: true,
-            dismissAfter: 3000
-          });
     };
 
   render(){
@@ -65,11 +85,11 @@ class Content extends React.Component  {
             </Row>
             <Row>
             <Col>
-                <ul class="list-group content-wrap-text">
-                <li class="list-group-item list-group-item-action list-group-item-light">Connected: <strong><span id="inner1"></span></strong></li>
-                <li class="list-group-item list-group-item-action list-group-item-light">All: <strong><span id="inner2"></span></strong></li>
-                <li class="list-group-item list-group-item-action list-group-item-light">Detected: <strong><span id="inner3"></span></strong></li>
-                <li class="list-group-item list-group-item-action list-group-item-light">Active users: <strong><span id="inner4"></span></strong></li>
+                <ul className="list-group content-wrap-text">
+                <li className="list-group-item list-group-item-action list-group-item-light">Connected: {this.state.connected}<strong><span id="inner1"></span></strong></li>
+                <li className="list-group-item list-group-item-action list-group-item-light">All: {this.state.all}<strong><span id="inner2"></span></strong></li>
+                <li className="list-group-item list-group-item-action list-group-item-light">Detected: {this.state.detected}<strong><span id="inner3"></span></strong></li>
+                <li className="list-group-item list-group-item-action list-group-item-light">Active users: <strong><span id="inner4"></span></strong></li>
                 </ul>
             </Col>
             </Row>
--- src/cmxAPI.js
+++ src/cmxAPI.js
@@ -19,7 +19,21 @@ class cmxAPI {
             reader.readAsDataURL(response.data);
         })
     }
-
+    
+    async getAllClients() {
+        const resp = await axios.get(this.cmxUrl + "/api/location/v2/clients ", {
+            auth: this.cmxAuth
+        })
+        return resp.data;
+    }
+    displayConnectedCount(floorName){
+        axios.get(this.cmxUrl + "/api/analytics/v1/now/connectedDetected", {
+            auth: this.cmxAuth
+        }).then(response => {
+            var res = response.find(e => e.area == floorName);
+            console.log("uy", res);
+        })
+    }
 }
 
 export default new cmxAPI()
